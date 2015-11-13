47d31adaf882d5a15ecac8e16bf4cc153833509f
vova tymets <volodymyrtymets@gmail.com>
2015-11-13 16:58:25 +0200

    add vk authorization

--- app/server/accounts/authorizations.js
+++ app/server/accounts/authorizations.js
@@ -1,9 +1,8 @@
-
-//Accounts.onCreateUser(function(options, user) {
-//    if (!user.profile) user.profile = {};
-//    user.profile.photo = (user.services && user.services.vk) ?
-//        user.services.vk.photo : null;
-//    user.profile.name = (user.services && user.services.vk) ?
-//    user.services.vk.first_name + ' ' +user.services.vk.last_name  : null;
-//    return user;
-//});
+Accounts.onCreateUser(function(options, user) {
+    if (!user.profile) user.profile = {};
+    user.profile.photo = (user.services && user.services.vk) ?
+        user.services.vk.photo : null;
+    user.profile.name = (user.services && user.services.vk) ?
+    user.services.vk.first_name + ' ' +user.services.vk.last_name  : null;
+    return user;
+});
