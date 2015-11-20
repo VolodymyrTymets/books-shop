9780107e954b62bc3e5ca219ce8a903a06c3e552
vova tymets <volodymyrtymets@gmail.com>
2015-11-20 15:23:00 +0200

    add image fixtures

--- app/lib/_fixtures.js
+++ app/lib/_fixtures.js
@@ -17,6 +17,29 @@ getAssets = function(assetsDir) {
         return EJSON.parse(Assets.getText(filePath));
     });
 };
+getFiles = function(assetsDir){
+    var assetsPath, files, fullPath;
+    assetsPath = path.join(Meteor.rootPath, 'assets','app');
+    fullPath = path.join(assetsPath, assetsDir);
+    try {
+        files = fs.readdirSync(fullPath);
+
+    } catch (_error) {
+        console.log(_error.message)
+        console.log("Can't load fixtures from directory '" + assetsDir + "'!");
+        return assets;
+    }
+    return _.map(files, function(fileName) {
+        var filePath;
+        filePath = path.join(assetsDir, fileName);
+        var newFile = new FS.File();
+        newFile.attachData(Assets.getBinary(filePath), {type: 'image/png'});
+        newFile.name(fileName);
+        newFile._id = fileName
+        return newFile;
+    });
+
+}
 
 fillCollection = function(collection, assets) {
     if (!collection.find().count()) {
@@ -53,13 +76,24 @@ Meteor.startup(function() {
         if (collection.find().count()) {
             return;
         }
-
-        assetsDir = path.join(fixturesPath, assetsDir);
-        console.log('assetsDir -> ',assetsDir)
-        assets = getAssets(assetsDir);
-        if (assets.length) {
-            fillCollection(collection, assets);
-            return logAction(assets.length, collectionName);
+        if(collectionName !== 'Images'){
+            assetsDir = path.join(fixturesPath, assetsDir);
+            console.log('assetsDir -> ',assetsDir)
+            assets = getAssets(assetsDir);
+            if (assets.length) {
+                fillCollection(collection, assets);
+                return logAction(assets.length, collectionName);
+            }
+        }else{
+            debugger
+            assetsDir = path.join(fixturesPath, assetsDir);
+            console.log('assetsDir  [Image]-> ',assetsDir);
+             var  files = getFiles(assetsDir);
+            _.each(files,function (file) {
+                Images.insert(file);
+            });
         }
+
+
     });
 });
