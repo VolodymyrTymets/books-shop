3532e9b5fe1a7ce0019db06b238a01f4edd0a577
vova tymets <volodymyrtymets@gmail.com>
2015-11-20 16:55:30 +0200

    add localization buttons

--- app/client/templates/navbar/navbar.js
+++ app/client/templates/navbar/navbar.js
@@ -9,8 +9,8 @@ Template.NavBar.helpers({
 
 Template.NavBar.events({
     'click a[data-action="change-language"]': function(e,tmp){
-        //TAPi18n.setLanguage(e.target.attributes['data-name'].nodeValue);
-        //accountsUIBootstrap3.setLanguage(e.target.attributes['data-name'].nodeValue);
+        TAPi18n.setLanguage(e.target.attributes['data-name'].nodeValue);
+        accountsUIBootstrap3.setLanguage(e.target.attributes['data-name'].nodeValue);
     },
     'submit #search-form':function(e,tmp){
         e.preventDefault();
